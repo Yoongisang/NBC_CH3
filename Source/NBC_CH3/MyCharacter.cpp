22d7d5764b677e67c0e5c3b5d8c0d8f1b6bdd2d9
Yoongisang <sawsd@naver.com>
2026-01-20 20:02:42 +0900

    20260120 - Add component initialization and input mapping to MyCharacter
    컴포넌트 셋팅 & 인풋 매핑

--- Source/NBC_CH3/MyCharacter.cpp
+++ Source/NBC_CH3/MyCharacter.cpp
@@ -1,13 +1,36 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "MyCharacter.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+
 
 // Sets default values
 AMyCharacter::AMyCharacter()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	
+	// 캡슐(루트 컴포넌트)
+	CapsuleComp = CreateDefaultSubobject<UCapsuleComponent>(TEXT("CapusuleComp"));
+	RootComponent = CapsuleComp;
+
+	// 메시
+	MeshComp = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("MeshComp"));
+	MeshComp->SetupAttachment(RootComponent);
+	
+	// 스프링 암
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComp"));
+	SpringArmComp->SetupAttachment(RootComponent);
+	
+	// 카메라
+	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComp"));
+	CameraComp->SetupAttachment(SpringArmComp);
 
 }
 
@@ -15,7 +38,24 @@ AMyCharacter::AMyCharacter()
 void AMyCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	// Physics false
+	if (CapsuleComp->IsSimulatingPhysics())
+	{
+		CapsuleComp->SetSimulatePhysics(false);
+	}
+
+	if (MeshComp->IsSimulatingPhysics())
+	{
+		MeshComp->SetSimulatePhysics(false);
+	}
+
+	if (APlayerController* PC = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PC->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(DefaultMappingContext, 0);
+		}
+	}
 }
 
 // Called every frame
--- Source/NBC_CH3/MyCharacter.h
+++ Source/NBC_CH3/MyCharacter.h
@@ -1,10 +1,16 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
 #include "MyCharacter.generated.h"
+
+class UCapsuleComponent;
+class USkeletalMeshComponent;
+class USpringArmComponent;
+class UCameraComponent;
+class UInputMappingContext;
 
 UCLASS()
 class NBC_CH3_API AMyCharacter : public APawn
@@ -18,7 +24,21 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
+	// 캡슐 컴포넌트
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Component")
+	TObjectPtr<UCapsuleComponent> CapsuleComp;
+	// 메시 컴포넌트
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Component")
+	TObjectPtr<USkeletalMeshComponent> MeshComp;
+	// 스프링 암 컴포넌트
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Component")
+	TObjectPtr<USpringArmComponent> SpringArmComp;
+	// 카메라 컴포넌트
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Component")
+	TObjectPtr<UCameraComponent> CameraComp;
+	// InputMappingContext
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	TObjectPtr<UInputMappingContext> DefaultMappingContext;
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;

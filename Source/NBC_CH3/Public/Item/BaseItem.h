cbd4b49ebcc19af0581c7214227f27125b6fbc75
Yoongisang <sawsd@naver.com>
2026-02-02 19:15:32 +0900

    20260202 - Add initial classes and map
    클래스 기본 구조 구현

---
+++ Source/NBC_CH3/Public/Item/BaseItem.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ItemInterface.h"
+#include "BaseItem.generated.h"
+
+UCLASS()
+class NBC_CH3_API ABaseItem : public AActor, public IItemInterface
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABaseItem();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
---
+++ Source/NBC_CH3/Public/Item/CoinItem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Item/BaseItem.h"
+#include "CoinItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NBC_CH3_API ACoinItem : public ABaseItem
+{
+	GENERATED_BODY()
+	
+};
